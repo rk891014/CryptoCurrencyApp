6e70852d346b9dc507a2a7461d35c3445b4b8563
rk891014 <rkrjptrohit@gmail.com>
2025-01-10 11:36:22 +0530

    done

--- app/src/main/java/com/example/cheqapp/presentation/coin_details/CoinDetailViewModel.kt
+++ app/src/main/java/com/example/cheqapp/presentation/coin_details/CoinDetailViewModel.kt
@@ -25,7 +25,6 @@ class CoinDetailViewModel @Inject constructor(
     init {
         savedStateHandle.get<String>(Constants.PARAM_COIN_ID)?.let {coinId ->
             getCoin(coinId)
-
         }
     }
 
